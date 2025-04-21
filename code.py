6ce33d428a86c5b6e63809b9dd20122d482a107e
Taylor Faragher <faragher6@gmail.com>
2025-04-20 20:56:39 -0500

    Updates main script to display no image on load; set last temperature on failure

--- code.py
+++ code.py
@@ -10,6 +10,8 @@ from weather_api import fetch_weather_data
 from get_temp_color import get_temp_color
 
 FETCH_INTERVAL_SECONDS = 900 # 15 minutes
+GLOBAL_TEMPERATURE_VARIABLE = 0
+GLOBAL_DESCRIPTION_VARIABLE = ""
 
 # --- Display setup ---
 matrix = Matrix(width=64, height=32, bit_depth=4)
@@ -91,7 +93,7 @@ def set_error():
 def clear_error():
     error_area.text = ""
 
-def clean_condition(condition, is_day):
+def clean_condition(condition):
     condition_mapping = {
         "01d":"Sunny",
         "02d":"Clear",
@@ -123,18 +125,29 @@ def load_weather_image(condition):
                 group.remove(current_weather_tile_grid)
         except ValueError:
             pass
-    if condition is None:
+    if condition == "none":
+        current_weather_tile_grid = None
         return
 
-    image_key = WEATHER_IMAGES.get(condition, "/images/sunny.bmp")
-    if condition is not "empty":
-        bitmap = displayio.OnDiskBitmap(image_key)
-        tile_grid = displayio.TileGrid(bitmap, pixel_shader=bitmap.pixel_shader, tile_width=32, tile_height=32, width=1, height=1)
-        group.append(tile_grid)
-        current_weather_tile_grid = tile_grid
+    if condition is not None:
+        image_key = WEATHER_IMAGES.get(condition, "/images/sunny.bmp")
+        if condition is not "empty":
+            bitmap = displayio.OnDiskBitmap(image_key)
+            tile_grid = displayio.TileGrid(bitmap, pixel_shader=bitmap.pixel_shader, tile_width=32, tile_height=32, width=1, height=1)
+            group.append(tile_grid)
+            current_weather_tile_grid = tile_grid
+        else:
+            current_weather_tile_grid = None
+
+def set_loading_state():
+    clear_error()
+    status_area.text = "Loading..."
+    temperature_area.text = ""
+    description_area.text = ""
+    load_weather_image("none")
 
 while True:
-    clear_error()
+    set_loading_state()
     weather_data = fetch_weather_data(status_area, network_client=network)
 
     if weather_data:
@@ -149,16 +162,24 @@ while True:
         text = "{}°F".format(temperature)
         
         # Clean up the description
-        cleanDescription = clean_condition(icon, True)
+        cleanDescription = clean_condition(icon)
         
         # Load the weather image
         load_weather_image(cleanDescription)
         
+        # Update global variables
+        GLOBAL_TEMPERATURE_VARIABLE = text
+        GLOBAL_DESCRIPTION_VARIABLE = cleanDescription
         # Update text areas
         temperature_area.text = text
         description_area.text = cleanDescription
     else:
+        # Turn on error state
         set_error()
+        # Set text to saved global variables
+        temperature_area.text = GLOBAL_TEMPERATURE_VARIABLE
+        description_area.text = GLOBAL_DESCRIPTION_VARIABLE
+        load_weather_image(cleanDescription)
         print("Failed to fetch data")
 
     time.sleep(FETCH_INTERVAL_SECONDS)
