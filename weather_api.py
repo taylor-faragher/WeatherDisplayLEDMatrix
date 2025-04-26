b62e8ec7b9eac2f6a1d3fbe5cc8fad00d532700c
Taylor Faragher <faragher6@gmail.com>
2025-04-26 18:23:40 -0500

    Updates api call to have larger timeout; sets LED to green on success

--- weather_api.py
+++ weather_api.py
@@ -1,8 +1,4 @@
-# from adafruit_matrixportal.network import Network
-# import board
 
-# network = Network(status_neopixel=board.NEOPIXEL, debug=False)
-# network.connect()
 
 API_URL = "https://api.taylorsweatherapi.com/?zipcode=37167"
 
@@ -15,7 +11,8 @@ HEADERS = {
 def fetch_weather_data(status_display_object, network_client):
     try:
         
-        response = network_client.fetch(API_URL, headers=HEADERS)
+        response = network_client.fetch(API_URL, headers=HEADERS, timeout=30)
+        network_client.check_response(response) #should set the status LED to green
 
         data = response.json()
 
@@ -30,6 +27,7 @@ def fetch_weather_data(status_display_object, network_client):
 
     except (ValueError, RuntimeError, KeyError, ConnectionError) as e:
         error_msg = f"Fetch Error: {str(e)}"
+        network_client.check_response(response) #should set the status LED to red
         status_display_object.text = ""
         print(error_msg) 
         return None
