7d9560235ef5de85e83c9727f3a3a49ffc2b05f0
Taylor Faragher <faragher6@gmail.com>
2025-04-30 22:38:23 -0500

    Replaces check_response with neo_tatus; we just want to change the color

--- weather_api.py
+++ weather_api.py
@@ -10,9 +10,8 @@ HEADERS = {
 
 def fetch_weather_data(status_display_object, network_client):
     try:
-        
         response = network_client.fetch(API_URL, headers=HEADERS, timeout=30)
-        network_client.check_response(response) #should set the status LED to green
+        network_client.neo_status(value=(0, 100, 0)) #should set the status LED to green
 
         data = response.json()
 
@@ -27,7 +26,7 @@ def fetch_weather_data(status_display_object, network_client):
 
     except (ValueError, RuntimeError, KeyError, ConnectionError) as e:
         error_msg = f"Fetch Error: {str(e)}"
-        network_client.check_response(response) #should set the status LED to red
+        network_client.neo_status(value=(100, 0, 0)) #should set the status LED to red
         status_display_object.text = ""
         print(error_msg) 
         return None
